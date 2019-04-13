9b3b4b03e0e899a18dccae32527ce37def2c0e91
Dickens-odera <dickensodera9@gmail.com>
2019-04-13 16:02:11 +0300

    Adding more api endpoints

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,15 @@
     <div class="col-md-2"></div>
     <div class="col-md-8">
         <div class="jumbotron text-center">
-            <h4 class="jumbtron-header">
+            <h4 class="jumbotron-header">
                 LARAVEL BLOG
         </h4>
                 <small>Building a RESTFUL api powered blog with laravel</small><br>
-            <a href="{{ route('register')}}" class="btn btn-success btn-lg">Get Started</a>
+                @if(@!Auth::user())
+                     <a href="{{ route('register')}}" class="btn btn-success btn-lg">Get Started</a>
+                @else
+                    <a href="{{  url('posts')  }}" class="btn btn-primary">Continue</a>
+                @endif
         </div>
     </div>
     <div class="col-md-2"></div>
